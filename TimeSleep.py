2cdc0e17ddba24566a6d331f3f0b19ed0ae0458b
Gilmarsantosfilho <99095164+Gilmarsantosfilho@users.noreply.github.com>
2022-02-06 14:36:33 -0300

    Create TimeSleep.py

---
+++ TimeSleep.py
@@ -0,0 +1,3 @@
+import time # O time()função retorna o número de segundos passados 
+
+time.sleep(2) #sleep() é definido no módulo time() do python 3. Às vezes, é necessário interromper o fluxo do programa para que várias outras execuções possam ocorrer ou simplesmente devido ao utilitário necessário. sleep() pode ser útil em tal situação, fornecendo uma maneira precisa e flexível de interromper o fluxo de código por qualquer período de tempo. Esta função discute o insight desta função.
